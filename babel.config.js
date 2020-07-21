1444ab0704cb023bcbb482131969354d756f7c15
destinytaoer <1848765519@qq.com>
2020-07-21 22:07:02 +0800

    chore[all] 搭建目录结构,配置路径别名
    1. 搭建目录结构
    2. 安装 babel-plugin-module-resolver
    3. 配置路径别名

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,20 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: './src', // 绝对路径的根目录
+        alias: {
+          '@/utils': './src/utils',
+          '@/components': './src/components',
+          '@/assets': './src/assets',
+          '@/pages': './src/pages',
+          '@models': './src/models',
+          '@/navigator': './src/navigator',
+          '@/config': './src/config',
+        },
+      },
+    ],
+  ],
 };
